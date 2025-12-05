d85ef7695f9eeae8929a5b7eb7665e1d79354284
MH-Miner <sminer2@schools.nyc.gov>
2025-12-05 00:49:21 -0500

    added dna_simulation with main scene

--- dna_simulator/index.service.worker.js
+++ dna_simulator/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764913601|13447756709';
+const CACHE_VERSION = '1764913728|13575367265';
 /** @type {string} */
 const CACHE_PREFIX = 'DNA Simulation-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
